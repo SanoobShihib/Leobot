d2dce178eddb54bbc1bbcffa1fd13ac8e067425f
Sanoob Achu 🌝 <sanoobsanoop130@gmail.com>
2026-09-24 00:25:38 +0530

    Update Help.py

--- plugins/Help.py
+++ plugins/Help.py
@@ -1,9 +1,146 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
+HELP_TEXT = """
+<b>❗️How to Search Movies Here❓
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+1. Just Send Movie Name and Movie Released Year Correctly.
+
+<blockquote>
+Check Google for Correct Movie Spelling and Movie Released Year
+</blockquote>
+
+<b>Examples:</b>
+
+<code>Oppam 2016</code>
+<code>Baahubali 2015 1080p</code>
+
+<blockquote>
+For Getting only 1080p Quality Files
+</blockquote>
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+<code>Baahubali 2015 Malayalam</code>
+<code>Baahubali 2015 Tamil</code>
+
+<blockquote>
+For Dubbed Movie Files
+</blockquote>
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+❗️On Android, Better Use VLC Media Player For Watch Movie's.
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+Cᴏɴᴛᴀᴄᴛ Bᴏᴛ Dᴇᴠᴇʟᴏᴘᴇʀ
+(Oʀ) Rᴇᴘᴏʀᴛ Bᴜɢꜱ..!!
+
+👉 @clsupportgroup
+</b>
+"""
+
+
+# /help command
 @Client.on_message(filters.command("help"))
 async def generate_link(client, message):
-    command_text = message.text.split(maxsplit=1)
-    if len(command_text) < 2:
-        await message.reply("<b>❗️How to Search Movies Here❓\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n1. Just Send Movie Name and Movie Released Year Correctly.\n<blockquote>(Check Google for Correct Movie Spelling and Movie Released Year)</blockquote>\n\nExamples: -\nOppam 2016\nBaahubali 2015 1080p\n<blockquote>(For Getting only 1080p Quality Files)</blockquote>\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\nBaahubali 2015 Malayalam\nBaahubali 2015 Tamil\n<blockquote>(For Dubbed Movie Files)</blockquote>\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n❗️On Android, Better Use VLC Media Player For Watch Movie's.\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\nCᴏɴᴛᴀᴄᴛ Bᴏᴛ Dᴇᴠᴇʟᴏᴘᴇʀ (Oʀ) Rᴇᴘᴏʀᴛ Bᴜɢꜱ..!! 👉 @clsupportgroup</b>")
-        return
+
+    buttons = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "📖 How To Use",
+                    callback_data="help_howto"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "👨‍💻 Developer",
+                    url="https://t.me/clsupportgroup"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "🔙 Close",
+                    callback_data="help_close"
+                )
+            ]
+        ]
+    )
+
+    await message.reply(
+        "<b>👋 Welcome to Help Menu!</b>\n\n"
+        "Choose an option below 👇",
+        reply_markup=buttons
+    )
+
+
+# How To Use button
+@Client.on_callback_query(filters.regex("^help_howto$"))
+async def help_howto(client, query):
+
+    buttons = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "🔙 Back",
+                    callback_data="help_back"
+                )
+            ]
+        ]
+    )
+
+    await query.message.edit_text(
+        HELP_TEXT,
+        reply_markup=buttons
+    )
+
+    await query.answer()
+
+
+# Back button
+@Client.on_callback_query(filters.regex("^help_back$"))
+async def help_back(client, query):
+
+    buttons = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "📖 How To Use",
+                    callback_data="help_howto"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "👨‍💻 Developer",
+                    url="https://t.me/clsupportgroup"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "🔙 Close",
+                    callback_data="help_close"
+                )
+            ]
+        ]
+    )
+
+    await query.message.edit_text(
+        "<b>👋 Welcome to Help Menu!</b>\n\n"
+        "Choose an option below 👇",
+        reply_markup=buttons
+    )
+
+    await query.answer()
+
+
+# Close button
+@Client.on_callback_query(filters.regex("^help_close$"))
+async def help_close(client, query):
+
+    await query.message.delete()
+    await query.answer()
