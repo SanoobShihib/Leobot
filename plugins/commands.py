bf63301a9b0cf4d2fe88bc72ad9035f797f10118
Sanoob Achu 🌝 <sanoobsanoop130@gmail.com>
2026-09-24 14:54:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -125,13 +125,13 @@ async def start(client, message):
     # For Achu Vj
     if len(message.command) != 2 or message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ 𝙰𝚍𝚍 𝙼𝚎 𝚃𝚘 𝚈𝚘𝚞𝚛 𝙶𝚛𝚘𝚞𝚙𝚜 ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘꜱ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 𝚂𝚎𝚊𝚛𝚌𝚑', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 𝚄𝚙𝚍𝚊𝚝𝚎𝚜', url='https://t.me/Clmainchannel')
+            InlineKeyboardButton('🔍 ꜱᴇᴀʀᴄʜ 🔎', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('📣 ᴜᴘᴅᴀᴛᴇꜱ 📣', url='https://t.me/Clmainchannel')
             ],[
-            InlineKeyboardButton('ℹ️ 𝙷𝚎𝚕𝚙', callback_data='help'),
-            InlineKeyboardButton('😊 𝙰𝚋𝚘𝚞𝚝', callback_data='about')
+            InlineKeyboardButton('ℹ️ ʜᴇʟᴘ ℹ️', callback_data='help'),
+            InlineKeyboardButton(`💡 ᴀʙᴏᴜᴛ 💡', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         caption_text = script.START_TXT.format(
