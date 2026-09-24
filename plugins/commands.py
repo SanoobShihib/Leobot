77c349c21f8bf39a6436fd15a8ebd240de248a77
podimonv3 <145585918+podimonv3@users.noreply.github.com>
2026-09-24 12:02:20 +0530

    Refactor imports and add database stats commands

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,24 +1,36 @@
-import os
+import base64
+import json
 import logging
+import os
 import random
+import re
 import sys
 import asyncio
-from Script import script
-from pyrogram import Client, filters, enums
-from pyrogram.errors import ChatAdminRequired, FloodWait, MessageDeleteForbidden
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from asyncio import sleep
-from pyrogram.enums import ChatType
-from database.ia_filterdb import Media, Mediaa, get_file_details, unpack_new_file_id, delete_files_below_threshold
-from database.users_chats_db import db
-from info import CHANNELS, ADMINS, REQ_CHANNEL1, REQ_CHANNEL2, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, DATABASE_URI, DATABASE_NAME, START_IMG, START_VID
-from utils import get_settings, get_size, is_subscribed, is_requested_one, is_requested_two, save_group_settings, temp, check_loop_sub, check_loop_sub1, check_loop_sub2
-from database.connections_mdb import active_connection
-from plugins.pm_filter import auto_filter
-import re
-import json
-import base64
+
 import pymongo
+from pyrogram import Client, enums, filters
+from pyrogram.enums import ChatType
+from pyrogram.errors import ChatAdminRequired, FloodWait, MessageDeleteForbidden
+from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+from Script import script
+from database.connections_mdb import active_connection
+from database.ia_filterdb import Media, Mediaa, get_file_details, unpack_new_file_id, delete_files_below_threshold, db as clientDB, db1 as clientDB2, db2 as clientDB3
+from database.users_chats_db import db
+from info import (
+    ADMINS, CHANNELS, LOG_CHANNEL, REQ_CHANNEL1, REQ_CHANNEL2, SUPPORT_CHAT, 
+    MELCOW_NEW_USERS, PICS, START_IMG, START_VID, DATABASE_URI, DATABASE_NAME, 
+    PROTECT_CONTENT, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION
+)
+from plugins.pm_filter import auto_filter
+from utils import (
+    get_settings, get_size, is_subscribed, is_requested_one, is_requested_two, 
+    save_group_settings, temp, check_loop_sub, check_loop_sub1, check_loop_sub2
+)
+
+
 logger = logging.getLogger(__name__)
 
 from dotenv import load_dotenv
@@ -938,6 +950,63 @@ async def process_command(client, message):
 
     print(f'Total files deleted: {total_files_deleted}')
     await processing_message.edit_text(f'<b>Deletion complete: Deleted {total_files_deleted} files.</b>')
+
+
+@Client.on_message(filters.command('stats') & filters.incoming)
+async def get_ststs(bot, message):
+    rju = await message.reply('Fetching stats..')
+    tot = await Media.count_documents()
+    tota = await Mediaa.count_documents()
+    total = tot + tota
+    users = await db.total_users_count()
+    chats = await db.total_chat_count()
+    stats = await clientDB.command('dbStats')
+    used_dbSize = (stats['dataSize']/(1024*1024))+(stats['indexSize']/(1024*1024))        
+    free_dbSize = 512-used_dbSize
+    stats2 = await clientDB2.command('dbStats')
+    used_dbSize2 = (stats2['dataSize']/(1024*1024))+(stats2['indexSize']/(1024*1024))
+    free_dbSize2 = 512-used_dbSize2
+    stats3 = await clientDB3.command('dbStats')
+    used_dbSize3 = (stats3['dataSize']/(1024*1024))+(stats2['indexSize']/(1024*1024))
+    free_dbSize3 = 512-used_dbSize3
+    await rju.edit(script.STATUS_TXT2.format(total, tot, round(used_dbSize2, 2), round(free_dbSize2, 2), tota, round(used_dbSize3, 2), round(free_dbSize3, 2), users, chats, round(used_dbSize, 2), round(free_dbSize, 2)))
+
+@Client.on_message(filters.command('chats') & filters.user(ADMINS))
+async def list_chats(bot, message):
+        await message.reply_document('chats.txt', caption="List Of Chats")
+
+
+
+@Client.on_message(filters.command('purge_one') & filters.private & filters.user(ADMINS))
+async def purge_req_one(bot, message):
+    r = await message.reply("`processing...`")
+    await db.delete_all_one()
+    await r.edit("**Req db Cleared**" )
+
+
+@Client.on_message(filters.command('purge_two') & filters.private & filters.user(ADMINS))
+async def purge_req_two(bot, message):
+    r = await message.reply("`processing...`")
+    await db.delete_all_two()
+    await r.edit("**Req db Cleared**" )
+
+@Client.on_message(filters.command("totalreq") & filters.user(ADMINS))
+async def total_requests(bot, message): 
+    rju = await message.reply('Fetching stats..')
+    total_one = await db.get_all_one_count()
+    total_two = await db.get_all_two_count()
+    if REQ_CHANNEL1 != False: 
+        req_channel1 = await bot.get_chat(REQ_CHANNEL1)
+        req_channel1 = req_channel1.title
+    else:
+        req_channel1 = "REQ_CHANNEL1"
+    if REQ_CHANNEL2 != False:
+        req_channel2 = await bot.get_chat(REQ_CHANNEL2)
+        req_channel2 = req_channel2.title
+    else:
+        req_channel2 = "REQ_CHANNEL2"
+    await rju.edit(f"{req_channel1} : {total_one}\n{req_channel2} : {total_two}")
+
 
 @Client.on_message(filters.command("delete_duplicate") & filters.user(ADMINS))
 async def delete_duplicate_files(client, message):
